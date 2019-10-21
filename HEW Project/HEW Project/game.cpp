9e4d77d3252eaead2a23f27e42277373bf7875e0
Kanade <katsurakanade@gmail.com>
2019-10-21 23:06:10 +0900

    1.0.3 エラー修正

--- HEW Project/HEW Project/game.cpp
+++ HEW Project/HEW Project/game.cpp
@@ -5,9 +5,14 @@
 #include "Menu.h"
 #include "scene.h"
 
+LPDIRECT3DTEXTURE9 *TEXTUREARRAY = new LPDIRECT3DTEXTURE9[3];
+
+static Menu *main = new Menu(2);
+
 void Init_Game() {
 
-	
+	TEXTUREARRAY[0] = Texture_GetTexture(TEXTURE_INDEX_YUKIDARUMA);
+	TEXTUREARRAY[1] = Texture_GetTexture(TEXTURE_INDEX_YUKIDARUMA);
 
 }
 
@@ -17,14 +22,14 @@ void Uninit_Game() {
 
 void Update_Game() {
 
-	if (Keyboard_IsTrigger(DIK_R) || GamePad_IsTrigger(0,JOYCON_HOME)) {
+	if (Keyboard_IsTrigger(DIK_R)) {
 		Scene_Change(SCENE_INDEX_TITLE);
 	}
-
 
 }
 
 void Draw_Game() {
 
+	
 
 }
--- HEW Project/HEW Project/title.cpp
+++ HEW Project/HEW Project/title.cpp
@@ -1,23 +1,38 @@
 #include "title.h"
 #include "scene.h"
 #include "input.h"
+#include "GameObject.h"
+#include "Menu.h"
+#include "texture.h"
+
+LPDIRECT3DTEXTURE9 *Menu_TextureArray = new LPDIRECT3DTEXTURE9[3];
+static Menu  *main = new Menu(2);
 
 void Init_Title() {
+
+	Menu_TextureArray[0] = Texture_GetTexture(TEXTURE_INDEX_MENUSTART);
+	Menu_TextureArray[1] = Texture_GetTexture(TEXTURE_INDEX_MENUEXIT);
 
 }
 
 void Uninit_Title() {
 
+
 }
 
 void Update_Title() {
 
-	if (Keyboard_IsPress(DIK_SPACE) ) {
+	if (main->UpdateSelectMenu() == 0) {
 		Scene_Change(SCENE_INDEX_GAME);
+	}
+
+	else if (main->UpdateSelectMenu() == 1) {
+		exit(1);
 	}
 
 }
 
 void Draw_Title() {
 
+	main->Draw(Menu_TextureArray, 500, 400, 150, 256, 128);
 }
