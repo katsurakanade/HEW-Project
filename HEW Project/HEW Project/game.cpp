ce1e076fd77c06a87593c8ab38cad4d41b4c4968
Kanade <katsurakanade@gmail.com>
2019-10-21 23:03:04 +0900

    1.0.2 エラー修正

--- HEW Project/HEW Project/game.cpp
+++ HEW Project/HEW Project/game.cpp
@@ -1,8 +1,13 @@
 #include "game.h"
 #include "GameObject.h"
 #include "input.h"
+#include "texture.h"
+#include "Menu.h"
+#include "scene.h"
 
 void Init_Game() {
+
+	
 
 }
 
@@ -12,7 +17,11 @@ void Uninit_Game() {
 
 void Update_Game() {
 
-	
+	if (Keyboard_IsTrigger(DIK_R) || GamePad_IsTrigger(0,JOYCON_HOME)) {
+		Scene_Change(SCENE_INDEX_TITLE);
+	}
+
+
 }
 
 void Draw_Game() {
